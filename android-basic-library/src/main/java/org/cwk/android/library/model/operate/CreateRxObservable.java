cf82e212b2b6e6350ab1290ccee0039a8fa430ef
超悟空 <39251474@163.com>
2017-07-19 18:40:52 +0800

    版本升级到0.5.8，微调，work RxAndroid 增加其他类型可订阅对象

--- android-basic-library/src/main/java/org/cwk/android/library/model/operate/CreateRxObservable.java
+++ android-basic-library/src/main/java/org/cwk/android/library/model/operate/CreateRxObservable.java
@@ -2,7 +2,10 @@ package org.cwk.android.library.model.operate;
 
 import org.cwk.android.library.model.data.WorkResult;
 
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 
 /**
  * 任务创建RxJava被订阅者对象的接口
@@ -22,4 +25,31 @@ public interface CreateRxObservable<Parameters, Result> {
      */
     @SuppressWarnings("unchecked")
     Observable<WorkResult<Result>> observable(Parameters... parameters);
+
+    /**
+     * 创建一个任务的{@link Maybe}对象，执行成功会发送onSuccess，失败发送onComplete，不会发送onError事件
+     *
+     * @param parameters 任务所需参数
+     *
+     * @return {@link Maybe}对象
+     */
+    Maybe<Result> maybe(Parameters... parameters);
+
+    /**
+     * 创建一个任务的{@link Single}对象，执行成功会发送onSuccess，失败发送onError
+     *
+     * @param parameters 任务所需参数
+     *
+     * @return {@link Single}对象
+     */
+    Single<Result> single(Parameters... parameters);
+
+    /**
+     * 创建一个任务的{@link Completable}对象，执行成功会发送onComplete，失败发送onError
+     *
+     * @param parameters 任务所需参数
+     *
+     * @return {@link Completable}对象
+     */
+    Completable completable(Parameters... parameters);
 }
--- android-basic-library/src/main/java/org/cwk/android/library/model/work/CreateRxObservableImp.java
+++ android-basic-library/src/main/java/org/cwk/android/library/model/work/CreateRxObservableImp.java
@@ -6,9 +6,18 @@ import org.cwk.android.library.model.data.IDefaultDataModel;
 import org.cwk.android.library.model.data.WorkResult;
 import org.cwk.android.library.model.operate.CreateRxObservable;
 
+import io.reactivex.Completable;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.Maybe;
+import io.reactivex.MaybeEmitter;
+import io.reactivex.MaybeOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
 import io.reactivex.functions.Cancellable;
 
 /**
@@ -58,6 +67,102 @@ class CreateRxObservableImp<T extends DefaultWorkModel<Parameters, Result, ? ext
                     @Override
                     public void onFinish(boolean state, Result data, String message) {
                         e.onNext(new WorkResult<>(state, message, data));
+                    }
+                }).beginExecute(parameters);
+            }
+        });
+    }
+
+    @SafeVarargs
+    @Override
+    public final Maybe<Result> maybe(final Parameters... parameters) {
+        return Maybe.create(new MaybeOnSubscribe<Result>() {
+            @Override
+            public void subscribe(final MaybeEmitter<Result> maybeEmitter) throws Exception {
+                // 创建任务实例
+                final T work = workClass.newInstance();
+
+                // 设置需要任务监听
+                maybeEmitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        if (work != null && !work.isCanceled()) {
+                            work.cancel();
+                        }
+                    }
+                });
+                work.setOnWorkFinishListener(false, new OnWorkFinishListener<Result>() {
+                    @Override
+                    public void onFinish(boolean state, Result data, String message) {
+                        if (state) {
+                            maybeEmitter.onSuccess(data);
+                        } else {
+                            maybeEmitter.onComplete();
+                        }
+                    }
+                }).beginExecute(parameters);
+            }
+        });
+    }
+
+    @SafeVarargs
+    @Override
+    public final Single<Result> single(final Parameters... parameters) {
+        return Single.create(new SingleOnSubscribe<Result>() {
+            @Override
+            public void subscribe(final SingleEmitter<Result> singleEmitter) throws Exception {
+                // 创建任务实例
+                final T work = workClass.newInstance();
+
+                // 设置需要任务监听
+                singleEmitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        if (work != null && !work.isCanceled()) {
+                            work.cancel();
+                        }
+                    }
+                });
+                work.setOnWorkFinishListener(false, new OnWorkFinishListener<Result>() {
+                    @Override
+                    public void onFinish(boolean state, Result data, String message) {
+                        if (state) {
+                            singleEmitter.onSuccess(data);
+                        } else {
+                            singleEmitter.onError(new Throwable(message));
+                        }
+                    }
+                }).beginExecute(parameters);
+            }
+        });
+    }
+
+    @SafeVarargs
+    @Override
+    public final Completable completable(final Parameters... parameters) {
+        return Completable.create(new CompletableOnSubscribe() {
+            @Override
+            public void subscribe(final CompletableEmitter completableEmitter) throws Exception {
+                // 创建任务实例
+                final T work = workClass.newInstance();
+
+                // 设置需要任务监听
+                completableEmitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        if (work != null && !work.isCanceled()) {
+                            work.cancel();
+                        }
+                    }
+                });
+                work.setOnWorkFinishListener(false, new OnWorkFinishListener<Result>() {
+                    @Override
+                    public void onFinish(boolean state, Result data, String message) {
+                        if (state) {
+                            completableEmitter.onComplete();
+                        } else {
+                            completableEmitter.onError(new Throwable(message));
+                        }
                     }
                 }).beginExecute(parameters);
             }
