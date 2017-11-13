89d54a2c4a001b00b39d95feb941efeabaca8c1b
超悟空 <39251474@163.com>
2017-11-13 17:29:05 +0800

    修复Global.getApplication签名bug，升级到0.8.1

--- android-basic-library/src/main/java/org/cwk/android/library/global/Global.java
+++ android-basic-library/src/main/java/org/cwk/android/library/global/Global.java
@@ -1,7 +1,6 @@
 package org.cwk.android.library.global;
 
 import android.app.Application;
-import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -61,7 +60,7 @@ public class Global {
      *
      * @return 返回Application对象
      */
-    public static Context getApplication() {
+    public static Application getApplication() {
         return global.application;
     }
 
