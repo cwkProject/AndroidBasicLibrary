f875d4f720af15cfab01599a6701f67eaa0d8b2e
超悟空 <39251474@163.com>
2018-07-21 16:54:42 +0800

    升级到0.9.0 重构网络库框架结构

--- android-basic-library/src/main/java/org/cwk/android/library/annotation/Delete.java
+++ android-basic-library/src/main/java/org/cwk/android/library/annotation/Delete.java
@@ -1,7 +1,5 @@
 package org.cwk.android.library.annotation;
 
-import org.cwk.android.library.work.StandardWorkModel;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -10,7 +8,7 @@ import java.lang.annotation.Target;
 
 /**
  * 标注网络请求为http delete方式{@link org.cwk.android.library.network.factory.NetworkType#DELETE
- * }，用于标记{@link StandardWorkModel#onTaskUri()}
+ * }，用于标记{@link org.cwk.android.library.work.WorkModel#onTaskUri()}
  *
  * @author 超悟空
  * @version 1.0 2017/2/10
--- android-basic-library/src/main/java/org/cwk/android/library/annotation/UploadStream.java
+++ android-basic-library/src/main/java/org/cwk/android/library/annotation/UploadStream.java
@@ -7,8 +7,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 标注网络请求为http上传{@link org.cwk.android.library.network.factory.NetworkType#UPLOAD_STREAM}用
- * 要上传的文件用"file"作为key，文件路径作为"value"
+ * 标注网络请求为http上传{@link org.cwk.android.library.network.factory.NetworkType#UPLOAD_STREAM},
+ * 用于标记{@link org.cwk.android.library.work.WorkModel#onTaskUri()},
+ * 用要上传的文件用"file"作为key，文件路径作为"value"
  *
  * @author 超悟空
  * @version 1.0 2017/8/25
